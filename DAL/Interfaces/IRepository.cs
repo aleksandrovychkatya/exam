3acefd7d514a2281eab73c42350ab8d7e12e4420
DESKTOP-SN1CSC4\Chamomilla <aleksandrovychkatya@gmail.com>
2020-06-26 09:09:28 +0300

    Добавьте файлы проекта.

---
+++ DAL/Interfaces/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        void Create(T item);
+        void Update(T item);
+    }
+  
+}
